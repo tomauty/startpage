ec031004abc635967d4f06c1807d90b052d3bf7d
Tom Auty <tauty@moneylion.com>
2018-03-25 12:50:33 -0400

    Add things

--- z.js
+++ z.js
@@ -4,6 +4,7 @@ const links = {
     "work": {
         "jira": "https://moneylion.atlassian.net",
         "confluence": "https://moneylion.atlassian.net/wiki",
+        "bamboo": "https://bamboo.moneylion.com",
         "stash": "https://stash.moneylion.com",
         "invision": "https://moneylion.invisionapp.com",
         "bugsnag": "https://bugsnag.com",
@@ -14,25 +15,34 @@ const links = {
     "$": {
         "google finance": "https://finance.google.com",
         "schwab": "https://schwab.com",
+        "ynab": "https://app.youneedabudget.com",
     },
     "news": {
         "tricycle": "https://tricycle.org",
+	"cabinet": "http://www.cabinetmagazine.org",
+	"hackernoon": "https://hackernoon.com",
         "hacker news": "https://news.ycombinator.com",
     },
     "friends": {
         "facebook": "https://facebook.com",
         "twitter": "https://twitter.com",
-        "hipinion": "https://forums.hipinion.com",
+        "hipinion": "http://forums.hipinion.com",
     },
 };
+
+function googleSearch(form) {
+    const searchText = encodeURIComponent(document.getElementById('search').value);
+    console.log(searchText);
+    window.open('https://www.google.com/search?q=' + searchText, '_self', false);
+}
 
 function init() {
 
     // good morning
     const hour = new Date().getHours();
     let hourString = 'morning';
-    if (hour > 12 && hour < 18) { hourString = 'afternoon'; }
-    if (hour > 18) { hourString = 'evening'; }
+    if (hour >= 12 && hour < 18) { hourString = 'afternoon'; }
+    if (hour >= 18) { hourString = 'evening'; }
     document.getElementById("hourText").textContent = 'good ' + hourString;
 
     // what are you seeing
@@ -45,9 +55,15 @@ function init() {
     const linkList = document.getElementById("links");
     let section;
     Object.keys(links).forEach(sectionName => {
+
+        const sectionContainer = document.createElement('div');
+        sectionContainer.setAttribute('style', 'display: flex; flex-direction: column; margin-right: 50px;');
+
+        linkList.appendChild(sectionContainer);
+
         section = document.createElement('h2');
         section.innerHTML = sectionName;
-        linkList.appendChild(section);
+        sectionContainer.appendChild(section);
 
         Object.keys(links[sectionName]).forEach(linkName => {
             "use strict";
@@ -55,7 +71,7 @@ function init() {
             link.setAttribute('href', links[sectionName][linkName]);
             // link.setAttribute('target', '_blank');
             link.innerHTML = linkName;
-            linkList.appendChild(link);
+            sectionContainer.appendChild(link);
         });
     });
 
